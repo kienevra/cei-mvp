8b614826711b164e44055c800e3134df55d2223d
Leon Miriti <leonnjiru@gmail.com>
2025-12-18 05:08:43 -0800

    Alembic: merge heads (render + local)

---
+++ backend/alembic/versions/cff1f49a9d1c_merge_heads_render_local.py
@@ -0,0 +1,20 @@
+
+"""merge heads (render + local)
+
+Revision ID: cff1f49a9d1c
+Revises: 2b49362d2565, b6f59d20042b
+
+Create Date: 2025-12-18 04:17:51.106207
+
+"""
+revision = 'cff1f49a9d1c'
+down_revision = ('2b49362d2565', 'b6f59d20042b')
+branch_labels = None
+depends_on = None
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
